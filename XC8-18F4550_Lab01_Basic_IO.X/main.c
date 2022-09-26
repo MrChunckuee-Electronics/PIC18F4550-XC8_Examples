3462e90df0a50f1027d5ce380d275b4bf01bc6df
MrChunckuee <mrchunckuee.psr@gmail.com>
2022-09-26 01:07:37 -0500

    Lab01 - Uso de entradas y salidas

--- XC8-18F4550_Lab01_Basic_IO.X/main.c
+++ XC8-18F4550_Lab01_Basic_IO.X/main.c
@@ -4,47 +4,84 @@
  *
  *******************************************************************************
  * FileName:        main.c
- * Processor:       PIC18F4550
- * Complier:        XC8 v2.32
- * Company:         Industrias Alfatek
- * Author:          Pedro Sánchez Ramírez 
- * Description:
- *
+ * Processor:       PIC12F675
+ * Complier:        XC8 v2.36
+ * Author:          Pedro Sánchez (MrChunckuee)
+ * Blog:            http://mrchunckuee.blogspot.com/
+ * Email:           mrchunckuee.psr@gmail.com
+ * Description:     
  *******************************************************************************
  * Rev.         Date            Comment
- *  v0.0.1      08/07/2022      - Creacion del firmware
+ *  v0.0.1      26/09/2022      - Creacion del firmware
  ******************************************************************************/
 
+/************ I N C L U D E S *************************************************/
 #include <xc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h> 
+#include <stdint.h>
+#include <stdbool.h>
 #include "device_config.h"
 
+/********* G E N E R A L   D E F I N I T I O N S ******************************/
 #define _XTAL_FREQ 8000000
 
-#define LED1_pinStatus        LATBbits.LATB5
+/*********P O R T   D E F I N I T I O N S *************************************/
+#define LED1_pinStatus        LATEbits.LATE0
 #define LED1_pinStatus_SetHigh()    do { LED1_pinStatus = 1; } while(0)
 #define LED1_pinStatus_SetLow()     do { LED1_pinStatus = 0; } while(0)
 #define LED1_pinStatus_Toggle()     do { LED1_pinStatus = ~LED1_pinStatus; } while(0)
+#define BUTON1_pinState             PORTAbits.RA0
+#define BUTON1_pinState_GetValue()  BUTON1_pinState
 
-#define BOTON1_pinState             PORTAbits.RA0
-#define BOTON1_pinState_GetValue()  BOTON1_pinState
+/******* V A R I A B L E S ****************************************************/
+bool BUTON_Status;
+uint8_t BUTON_Ticks;
 
+/*********** P R O T O T Y P E S **********************************************/
 void MCU_Initialize(void);
+void IO_Task(void);
 
 void main(void) {
     MCU_Initialize();
     while (1){
-        LED1_pinStatus_SetHigh();
+        IO_Task();
     }
     return;
 }
 
 void MCU_Initialize(void){
-    TRISA = 0b0000001;
-    TRISD = 0x00;
+    // Config IO
+    TRISE = 0b001;
+    PORTE = 0b000;
     
     //Confif OSC
     OSCCONbits.SCS = 0b11;
     OSCCONbits.IRCF = 0b111;
     OSCCONbits.IDLEN = 0;
     OSCCONbits.IOFS = 1;  
+    
+    BUTON_Status = 0;
+    BUTON_Ticks = 0;
+}
+
+void IO_Task(void){
+    // Read button status
+    while(BUTON1_pinState_GetValue()){
+        BUTON_Ticks++;
+        if(BUTON_Ticks>20){
+            BUTON_Ticks = 0;
+            BUTON_Status^=1;
+        }
+        __delay_ms(10);
+    }
+    
+    // Toggle LED depending BOTON_Status
+    if(BUTON_Status){
+        LED1_pinStatus_SetHigh();
+    }
+    else{
+        LED1_pinStatus_SetLow();
+    }
 }
--- XC8-18F4550_Lab01_Basic_IO.X/nbproject/Makefile-variables.mk
+++ XC8-18F4550_Lab01_Basic_IO.X/nbproject/Makefile-variables.mk
@@ -8,6 +8,3 @@ CND_BASEDIR=`pwd`
 CND_ARTIFACT_DIR_default=dist/default/production
 CND_ARTIFACT_NAME_default=XC8-18F4550_Lab01_Basic_IO.X.production.hex
 CND_ARTIFACT_PATH_default=dist/default/production/XC8-18F4550_Lab01_Basic_IO.X.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=xc8-18f4550lab01basicio.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/xc8-18f4550lab01basicio.x.tar
